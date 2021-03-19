be508713762b74c7bba5e01745b45df15b95b623
Yz4230 <yuzupon1480@gmail.com>
2021-03-19 22:45:15 +0900

    seting config

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 cat ./source.txt >> README.md
 git add README.md
-git commit --amend --no-edit
+git config --global user.email yuzupon1480@gmail.com
+git config --global user.name Yz4230
+git commit -a -m "[Workflow] build"
 git push
